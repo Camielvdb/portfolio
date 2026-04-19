751862e4d2cc685b8e1d6abfc0786ddcf424104a
Camiel van der Beek <43265951+Camielvdb@users.noreply.github.com>
2026-04-19 18:03:12 -0400

    Add files via upload

--- scripts/render-work.js
+++ scripts/render-work.js
@@ -29,6 +29,47 @@
       front.appendChild(tokenizationVisual);
     }
 
+    if (item.frontClass === "kanban") {
+      var kanbanVisual = document.createElement("div");
+      var kanbanUiLayer = document.createElement("img");
+      var kanbanAbstractLayer = document.createElement("img");
+
+      kanbanVisual.className = "kanban-card-visual";
+      kanbanVisual.setAttribute("aria-hidden", "true");
+
+      kanbanUiLayer.className = "kanban-card-layer kanban-card-layer--ui";
+      kanbanUiLayer.src = "./images/honeybook-kanban-ui.png";
+      kanbanUiLayer.alt = "";
+      kanbanUiLayer.decoding = "async";
+
+      var kanbanAbstractLayerLight = document.createElement("img");
+      var kanbanAbstractLayerDark = document.createElement("img");
+
+      kanbanAbstractLayer.className = "kanban-card-layer kanban-card-layer--abstract kanban-card-layer--abstract-light";
+      kanbanAbstractLayer.src = "./images/honeybook-kanban-abstract-light.png";
+      kanbanAbstractLayer.alt = "";
+      kanbanAbstractLayer.decoding = "async";
+
+      kanbanAbstractLayerDark.className = "kanban-card-layer kanban-card-layer--abstract kanban-card-layer--abstract-dark";
+      kanbanAbstractLayerDark.src = "./images/honeybook-kanban-abstract-dark.png";
+      kanbanAbstractLayerDark.alt = "";
+      kanbanAbstractLayerDark.decoding = "async";
+
+      kanbanVisual.appendChild(kanbanAbstractLayer);
+      kanbanVisual.appendChild(kanbanAbstractLayerDark);
+      kanbanVisual.appendChild(kanbanUiLayer);
+
+      var kanbanSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      var kanbanDividerLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+      kanbanSvg.setAttribute("class", "kanban-divider-svg");
+      kanbanSvg.setAttribute("aria-hidden", "true");
+      kanbanDividerLine.setAttribute("class", "kanban-divider-line");
+      kanbanSvg.appendChild(kanbanDividerLine);
+
+      front.appendChild(kanbanVisual);
+      front.appendChild(kanbanSvg);
+    }
+
     var titleContainer = document.createElement("div");
     titleContainer.className = "item-title-container";
 
